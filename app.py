6bae5edd579b0e7a74e76acf122969c9188d58ed
Patricia Zych <tyska@Tyskas-MacBook-Pro.local>
2025-03-18 14:57:45 -0500

    Moved dataset to 'data' folder and updated app.py

--- app.py
+++ app.py
@@ -2,4 +2,19 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-df = pd.read_csv("/Users/tyska/DataProjects/carsalesads/vehicles_us.csv")
+df = pd.read_csv("data/vehicles_us.csv")
+
+st.title("Car Advertisement Data Dashboard")
+
+st.header("Exploratory Data Analysis of Vehicle Prices and Mileage")
+
+if st.checkbox("Show raw data"):
+    st.write(df.head())
+
+st.subheader("Car Price Distribution")
+fig = px.histogram(df, x="price", nbins=30, color_discrete_sequence=["orange"], title="Distribution of Car Prices")
+st.plotly_chart(fig)
+
+st.subheader("Mileage vs Price")
+fig = px.scatter(df, x="odometer", y="price", color_discrete_sequence=["green"], title="Mileage vs Price")
+st.plotly_chart(fig)
